58d17caedfcff269765e79844d6dcd723c49f3c7
den-chan <den-chan@tuta.io>
2015-06-20 07:28:09 +1000

    Added blog txt file.

--- app.rb
+++ app.rb
@@ -2,8 +2,23 @@ require 'sinatra/base'
 
 module Moonshine
   class App < Sinatra::Base
+    set :static => false
+    
     get "/" do
-      send_file File.join(settings.public_folder, 'index.html')
+      send_file File.join(settings.public_folder, "index.html")
+    end
+    
+    get "/one.txt" do
+      response.headers["Access-Control-Allow-Origin"] = "http://den-chan.github.io"
+      response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+      response.headers["Content-Type"] = "text/plain;charset=utf-8"
+      send_file File.join(settings.public_folder, "one.txt")
+    end
+    
+    options "/one.txt" do
+      response.headers["Access-Control-Allow-Origin"] = "http://den-chan.github.io"
+      response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+      response.headers["Content-Type"] = "text/plain;charset=utf-8"
     end
   end
 end
